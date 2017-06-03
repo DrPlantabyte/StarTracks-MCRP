87600f024d3978432cfb9c877068e5c21aa47c54
DrCyano <explosivegnome@yahoo.com>
2017-06-03 09:27:59 -0400

    Project creation and refining of build script

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # StarTracks-MCRP
 This is a Sci-Fi themed resource pack for Minecraft.
+
+# Description
+This resource pack changes the story of Minecraft from that of a survivor in a pre-modern world to that of a space-human exploring a strange new planet, one whose peace is threatened by evil cyborgs and alien invaders. 
+
+Will you free the bird-like people of this once-great world? Or conquer them in the name of humanity?
+
+# Installation
+Simply download one of the release .zip files and put it in the *resourcepacks* folder inside your Minecraft profile folder (default location is *~/.minecraft* where ~ is either your home directory (unix-like computers) or *AppData/Roaming* (windows computers).
+## Building the resource pack
+To compile this resource pack, you must have the following programs installed on your computer:
+- Python 3+
+- Inkscape
+With these programs installed, you need only run the *build.py* python script to generate the resource pack .zip files, which will appear in the *distributables* folder. 
+
+# License and Redistribution
+This work is licensed under a Creative Commons Attribution-ShareAlike 4.0 International License (see http://creativecommons.org/licenses/by-sa/4.0/ ).
+
+You may use this resource pack in your own Minecraft games and may include it in a publically available resource pack or server. You are free to redistribute this resource pack however you want. You have my permission to change this resource pack and use it in your own works, so long as my name (either "Cyanobacterium", "DrCyano", or "Chris Hall") is listed as a co-author and your derivative work is also licensed under a creative commons license.
---
+++ build.py
@@ -0,0 +1,117 @@
+import shutil
+import sys
+import os
+import subprocess
+import hashlib
+import zipfile
+
+# executable locations
+from sys import platform
+if platform == 'linux' or platform == 'linux2':
+	# linux
+	inkscape_path = 'inkscape'
+elif platform == 'darwin':
+	# OS X (I don't actually know where OSX puts executables these days)
+	inkscape_path = 'inkscape'
+elif platform == 'win32' or platform == 'win64':
+	# Windows...
+	inkscape_path = 'C:\\Program Files\\Inkscape\\inkscape.exe'
+
+
+this_dir = os.path.dirname(os.path.realpath(__file__))
+
+tex_resolutions = [16, 32, 64, 128]
+project_dirs=[]
+
+def main():
+	remakeDir('distributables')
+	for res in tex_resolutions:
+		# setup build dir
+		src = 'x'+str(res)
+		src_dir = str(this_dir) + os.sep + src
+		svg_dir = str(this_dir) + os.sep + 'svg'
+		com_dir = str(this_dir) + os.sep + 'common'
+		build_dir = str(this_dir) + os.sep + 'build' + os.sep + src
+		zip_file = 'distributables' + os.sep + 'StarTracks_' + src + '.zip'
+		remakeDir(build_dir)
+		# copy common files
+		copyInto(src=com_dir, dst=build_dir)
+		# compile SVG textures
+		convertSVGDir(svg_dir, build_dir, res)
+		# copy raw textures
+		copyInto(src=src_dir, dst=build_dir)
+		# make distributable .zip files
+		the_files = listFiles(build_dir)
+		zipFiles(build_dir, the_files, zip_file, zipfile.ZIP_STORED)
+		# calculate sha1 hash for servers
+		hasher = hashlib.sha1()
+		with open(zip_file, 'rb') as f:
+			while True:
+				data = f.read(4096)
+				if not data:
+					break
+				hasher.update(data)
+		sha1_hash = hasher.hexdigest()
+		fout = open(zip_file+'_sha1.txt','w')
+		fout.write(sha1_hash)
+		fout.close()
+		# done
+
+def zipFiles(source_root, file_list, dest_file, compression):
+	# note: Minecraft is bad at handling compressed zip files
+	zout = zipfile.ZipFile(dest_file, mode='w', compression=compression, allowZip64=True)
+	try:
+		for filename in file_list:
+			input_file = str(source_root) + os.sep + str(filename)
+			zipped_file = str(filename)
+			zout.write(input_file, arcname=zipped_file)
+	finally:
+		zout.close()
+
+def listFiles(root_dir):
+	fl = []
+	for root, dirs, files in os.walk(root_dir):
+		rel_dirpath = os.path.relpath(root, start=root_dir)
+		for f in files:
+			rel_filepath = rel_dirpath + os.sep + f
+			fl.append(rel_filepath)
+	return fl
+def remakeDir(dirpath):
+	"""deletes and recreates a directory"""
+	if (os.path.exists(dirpath)):
+		for root_dir, dirs, files in os.walk(dirpath):
+			for f in files:
+				os.remove(root_dir + os.sep + f)
+		for root_dir, dirs, files in os.walk(dirpath):
+			for d in dirs:
+				os.remove(root_dir + os.sep + d)
+	os.makedirs(dirpath, exist_ok=True)
+def makeParentDir(dirpath):
+	parent_path =os.path.dirname(os.path.realpath(dirpath))
+	os.makedirs(parent_path, exist_ok=True)
+def copyInto(src, dst):
+	for root, dirs, files in os.walk(src):
+		rel_dirpath = os.path.relpath(root, start=src)
+		for f in files:
+			dst_filepath = dst + os.sep + rel_dirpath + os.sep + f
+			src_filepath = root + os.sep + f
+			makeParentDir(dst_filepath)
+			shutil.copyfile(src_filepath, dst_filepath)
+def convertSVGDir(src_dir, dest_dir, mc_resolution):
+	# use inkscape commandline
+	for root_dir, dirs, files in os.walk(src_dir):
+		rel_dirpath = os.path.relpath(root_dir, start=src_dir)
+		for f in files:
+			if(f.endswith('.svg')):
+				# svg file detected
+				name = f
+				dst_filepath = dest_dir + os.sep + rel_dirpath + os.sep + name.replace('.svg','.png')
+				src_filepath = root_dir + os.sep + name
+				convertSVG(src_filepath, dst_filepath, mc_resolution)
+def convertSVG(src_filepath, dst_filepath, mc_resolution):
+	source_dpi = 96
+	scaler = mc_resolution / 16
+	dpi = source_dpi * scaler
+	makeParentDir(dst_filepath)
+	subprocess.call(['C:\\Program Files\\Inkscape\\inkscape.exe','--export-png', str(dst_filepath), '--export-area-page', '--export-dpi', str(dpi), str(src_filepath)])
+main()

32206605744ea813457d302db1c6c8673c21725d
DrCyano <explosivegnome@yahoo.com>
2019-08-25 15:52:35 +1000

    arrow bug fix

--- modules/Rayguns/lang-convert.sh
+++ modules/Rayguns/lang-convert.sh
@@ -2,5 +2,5 @@
 cd "$(dirname "$0")"
 OUT='common/assets/minecraft/lang/en_us.json'
 SRC='../../reference/MC14.en_us.json'
-cat "${SRC}" | grep -E "{|[Ff]lint|[Bb]ow" | grep -E --invert-match "[Bb]owl|Projectile\:|debug|Parrot" | sed "s/ bow and arrow/ raygun/" | sed "s/Flint and Steel/Pocket Laser/" | sed "s/Crossbow/Plasma Cannon/" | sed "s/ crossbow/ plasma cannon/" | sed "s/Bow/Raygun/" | sed "s/Flint/Laser Crystal/" | sed "s/Arrow/Laserbolt/" > ${OUT}
+cat "${SRC}" | grep -E "{|[Ff]lint|[Bb]ow|Arrow" | grep -E --invert-match "[Bb]owl|Projectile\:|debug|Parrot" | sed "s/ bow and arrow/ raygun/" | sed "s/Flint and Steel/Pocket Laser/" | sed "s/Crossbow/Plasma Cannon/" | sed "s/ crossbow/ plasma cannon/" | sed "s/Bow/Raygun/" | sed "s/Flint/Laser Crystal/" | sed "s/Arrow/Laserbolt/" > ${OUT}
 echo '"_":"_" }' >> ${OUT}

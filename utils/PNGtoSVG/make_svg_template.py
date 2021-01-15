53719b8fdb1846535e4f42d8f168d558937eda72
DrCyano <explosivegnome@yahoo.com>
2021-01-15 12:48:24 +1100

    working on gui (working on utils)

--- utils/PNGtoSVG/make_svg_template.py
+++ utils/PNGtoSVG/make_svg_template.py
@@ -20,20 +20,42 @@ def main():
 		fail('not implemented yet')
 	else:
 		# non-recursive, single file
-		# TODO
-		fail('not implemented yet')
+		src = args.source
+		dst = args.destination
+		if not str(dst).lower().endswith('.svg'):
+			dst = str(dst) + '.svg'
+		img_to_SVG(src, dst)
 	pass
+
+def img_to_SVG(src_path, dst_path):
+	_print(src_path, '->', dst_path)
+	content = make_SVG(src_path)
+	with open(dst_path, 'w') as fout:
+		fout.write(content)
 
 def make_SVG(image_filepath):
 	img_type = (str(image_filepath)[str(image_filepath).rfind('.')+1:]).lower()
-	img = Image.open(image_filepath)
-	w, h = img.size
-	img_data = base64.b # stupid base64 library returns binary string on encode instead of encoding-agnostic string
+	if img_type == 'jpeg':
+		img_type = 'jpg'
+	if not (img_type == 'jpg' or img_type == 'png'):
+		fail('Unsupported image file type: %s' % img_type)
+	with Image.open(image_filepath) as img:
+		w, h = img.size
+	with open(image_filepath, 'rb') as fin:
+		img_data = bytes2b64(fin.read())
 	return _svg_template().format(img_type=img_type, img_width=w, img_height=h, img_base64=img_data)
+
+def bytes2b64(byte_array):
+	# stupid base64 library returns a utf-8 binary string on encode 
+	# instead of returning an encoding-agnostic string object
+	return base64.b64encode(byte_array).decode('utf8')
 
 def fail(msg):
 	print('Error:',msg, file=sys.stderr)
 	exit(1)
+
+def _print(*args, **kwargs):
+	print(*args, **kwargs)
 
 def _svg_template():
 	return '''<?xml version="1.0" encoding="UTF-8" standalone="no"?>
@@ -66,7 +88,7 @@ def _svg_template():
      inkscape:cx="0.0"
      inkscape:cy="0.0"
      inkscape:document-units="px"
-     inkscape:current-layer="layer1"
+     inkscape:current-layer="layer2"
      inkscape:document-rotation="0"
      showgrid="true"
      units="px"
